8f17bacca63acf9304a1cb1c098dbd526a847122
Josh C <41541186+open-analysis@users.noreply.github.com>
2022-11-06 10:31:55 -0600

    Moving project over to C#
    Tkinter isn't going to cut it, so moving over to something that'll produce a better end product.

---
+++ DnDCharacterSheet/Core/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DnDCharacterSheet.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> m_execute;
+        private Func<object, bool> m_canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value;  }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            m_execute = execute;
+            m_canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return m_canExecute == null || m_canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            m_execute(parameter);
+        }
+    }
+}
